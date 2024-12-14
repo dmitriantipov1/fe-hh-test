e804808bdfc5a01997d9e2d7fc1a3e39620e05e5
Dmitri <dmitri.antipov1@gmail.com>
2024-12-14 10:51:15 +0200

    Add .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-monaco-editor'],
+  runtimeConfig: {
+    public: {
+      BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
+    },
+  },
 })
